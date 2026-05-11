8a75731e946ca9f730e77267b8ac78c6648be21d
Tomas Kocourek <kocourek30@gmail.com>
2026-05-11 18:08:58 +0200

    Redirect site root to admin

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/admin/", permanent=False)),
     path("", include("apps.galleries.urls")),
     path("", include("apps.orders.urls")),
     path("admin/", admin.site.urls),
